016539ef9377a85702259273bc723983e6576dc3
SnGxl <fgxjyyssk@gmail.com>
2026-04-22 17:47:52 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 const SUPABASE_URL = 'https://cgiydhcrkwdjuoajefbj.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_nhFw6ucdW0t4_kEptMEkKg_QHONU-_E';
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 async function login() {
     const userField = document.getElementById('username').value;
@@ -35,5 +36,34 @@ async function login() {
         }
     } catch (error) {
         message.innerText = 'حدث خطأ في الاتصال بالسيرفر.';
+    }
+}
+async function signup() {
+    const userField = document.getElementById('username').value;
+    const passField = document.getElementById('password').value;
+    const message = document.getElementById('message');
+
+    if (!userField || !passField) {
+        message.innerText = "الرجاء إدخال البريد الإلكتروني وكلمة المرور";
+        message.style.color = 'orange';
+        return;
+    }
+
+    try {
+        // تنفيذ عملية التسجيل وإرسال الإيميل
+        const { data, error } = await _supabase.auth.signUp({
+            email: userField,
+            password: passField,
+        });
+
+        if (error) {
+            message.style.color = 'red';
+            message.innerText = "خطأ: " + error.message;
+        } else {
+            message.style.color = 'green';
+            message.innerText = "تم إرسال رابط التحقق! افحص بريدك الإلكتروني الآن.";
+        }
+    } catch (error) {
+        message.innerText = "حدث خطأ في الاتصال";
     }
 }
